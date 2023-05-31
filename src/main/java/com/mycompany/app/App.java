d9f25f5644e335e8d41feae331270088098d7fcf
ysp1g19 <yanshanphua@gmail.com>
2023-05-31 06:50:52 +0000

    Added all project files in first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final String MESSAGE = "I love Jenkins";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println("Today is such a hot day");
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
