977d21b09a1f17f7f13628e8ec96a3a0420acb94
ysp1g19 <yanshanphua@gmail.com>
2023-05-31 08:50:31 +0000

    The last change for today

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Today hahahaha");
+        System.out.println("Today yay!");
     }
 
     public String getMessage() {
