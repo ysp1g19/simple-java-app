d636106d77df05ecfb6101d27e44c2dfc902dbcb
ysp1g19 <yanshanphua@gmail.com>
2023-06-01 05:32:39 +0000

    Making a minor change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Lalala!");
+        System.out.println("Ohohooho!");
     }
 
     public String getMessage() {
