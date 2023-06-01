5ebddd167c200ab30ed4547502f03fbd2819b732
ysp1g19 <yanshanphua@gmail.com>
2023-06-01 05:29:13 +0000

    Making a change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Today yay!");
+        System.out.println("Lalala!");
     }
 
     public String getMessage() {
